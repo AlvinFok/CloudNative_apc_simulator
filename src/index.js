0795fcfe709f3b1a7d15780549c84f669c90102a
iYunghui <betty103800@gmail.com>
2022-06-01 20:15:02 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ const measureService = require('./measureService');
 const apcService = require('./apcService');
 const paramsService = require('./paramsService');
 
-const express = require('express')
-const Prometheus = require('prom-client')
+const express = require('express');
+const Prometheus = require('prom-client');
 
 let measureHandle = null;
 let paramsHandle = null;
 
-const app = express()
-const port = 8123
-const metricsInterval = Prometheus.collectDefaultMetrics()
+const app = express();
+const port = 8123;
+const metricsInterval = Prometheus.collectDefaultMetrics();
 const counter = new Prometheus.Counter({
   name: 'myapp_requests',
   help: 'Demo counter metric to record request count',
@@ -34,30 +34,30 @@ const gauge = new Prometheus.Gauge({
 
 app.get('/thickness', (req, res, next) => {
   counter.labels("thickness").inc();
-  gauge.labels("thickness").set(parseFloat(req.query.value))
-})
+  gauge.labels("thickness").set(parseFloat(req.query.value));
+});
 
 app.get('/moisture', (req, res, next) => {
   counter.labels("moisture").inc();
-  gauge.labels("moisture").set(parseFloat(req.query.value))
-})
+  gauge.labels("moisture").set(parseFloat(req.query.value));
+});
 
 app.get('/metrics', (req, res) => {
-  res.set('Content-Type', Prometheus.register.contentType)
-  res.end(Prometheus.register.metrics())
-})
+  res.set('Content-Type', Prometheus.register.contentType);
+  res.end(Prometheus.register.metrics());
+});
 
 // Error handler
 app.use((err, req, res, next) => {
-  res.statusCode = 500
+  res.statusCode = 500;
   // Do not expose your error in production
-  res.json({ error: err.message })
-  next()
-})
+  res.json({ error: err.message });
+  next();
+});
 
 const server = app.listen(port, () => {
   console.log(`listening on port ${port}`)
-})
+});
 
 const initGlobalNATSClient = async () => {
   // instantiate the nats client
@@ -90,7 +90,12 @@ const initGlobalCache = async () => {
   global.cache = new NodeCache();
 
   global.cache.set('FACTOR_THICKNESS', 0.5);
+  counter.labels("thickness").inc();
+  gauge.labels("thickness").set(0.5);
+
   global.cache.set('FACTOR_MOISTURE', 0.5);
+  counter.labels("moisture").inc();
+  gauge.labels("moisture").set(0.5);
 };
 
 const run = async () => {
@@ -126,12 +131,12 @@ process.on('SIGINT', async () => {
   }
 
   if(metricsInterval) {
-    clearInterval(metricsInterval)
+    clearInterval(metricsInterval);
   }
   
   server.close((err) => {
     if (err) {
-      console.error(err)
+      console.error(err);
     }
   })
 
